ddc0f19502a80794ed33236bc842f7c54e159a08
Dmytro <dmytro.matviichuk@plus4u.net>
2023-01-13 20:51:16 +0200

    fixed

--- main.py
+++ main.py
@@ -3,21 +3,21 @@ from classes.binary import BinaryFile
 from classes.buffer import BufferFile
 from classes.log_text import LogTextFile
 
-root = Directory('root', 100)
-child_dir1 = Directory('child_dir1', 10, root)
-child_dir2 = Directory('child_dir2', 5, child_dir1)
+main_dir = Directory('main_dir', 100)
+sub_dir1 = Directory('sub_dir1', 10, main_dir)
+sub_dir2 = Directory('sub_dir2', 5, sub_dir1)
 
-bin_file1 = BinaryFile(child_dir2, 'file1', 'text info')
-bin_file2 = BinaryFile(root, 'file2', 'another text info')
+bin_file1 = BinaryFile(sub_dir2, 'file1', 'text info')
+bin_file2 = BinaryFile(main_dir, 'file2', 'another text info')
 
-log_file1 = LogTextFile(child_dir1, 'file3', 'first line')
-log_file2 = LogTextFile(child_dir2, 'file4', 'foo')
+log_file1 = LogTextFile(sub_dir1, 'file3', 'first line')
+log_file2 = LogTextFile(sub_dir2, 'file4', 'foo')
 log_file1.append_line('second line')
 print(log_file1.get())
 
-buf_file1 = BufferFile(root, 10, 'file5')
-buf_file2 = BufferFile(child_dir1, 20, 'file6')
+buf_file1 = BufferFile(main_dir, 10, 'file5')
+buf_file2 = BufferFile(sub_dir1, 20, 'file6')
 
-buf_file2.changeOrder(child_dir2)
+buf_file2.changeOrder(sub_dir2)
 
-print(root.list_elems())
+print(main_dir.list_elems())
